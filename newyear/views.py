8e197074b4031ab81aa69b815c3a29de65632a29
johnmfwee <johnmfwee@gmail.com>
2021-10-28 16:58:14 +1100

    finished new year app

--- newyear/views.py
+++ newyear/views.py
@@ -8,5 +8,7 @@ from django.shortcuts import render
 def index(request):
     now = datetime.datetime.now()
     return render(
-        request, "newyear/index.html", {"newyear": now.month == 1 and now.day == 1}
+        request, "newyear/index.html", {
+            "newyear": True
+        }
     )
